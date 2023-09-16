f5658faca41d59a51c23b90ac430b1fa020389e4
DevitoDbug <Davidochiengy@gmail.com>
2023-09-16 10:20:35 +0300

    Read body of request

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	//https://api.openweathermap.org/data/2.5/weather?q=Nairobi&appid=73cbc8ffd789d41ed5535c3fdb8ca562
+	res, err := http.Get("https://api.openweathermap.org/data/2.5/weather?q=Nairobi&appid=73cbc8ffd789d41ed5535c3fdb8ca562")
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		panic("Weather api not available ")
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic("Could not read body of response")
+	}
+	fmt.Println(body)
+}
