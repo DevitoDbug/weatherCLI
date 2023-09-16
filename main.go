576458a7a2311ff13c97b5493e5bbf7f102719e9
DevitoDbug <Davidochiengy@gmail.com>
2023-09-16 10:51:59 +0300

    Create struct rep of json response

--- main.go
+++ main.go
@@ -6,6 +6,21 @@ import (
 	"net/http"
 )
 
+type Weather struct {
+	Name    string `json:"name"`
+	Weather []struct {
+		Description string `json:"description"`
+	} `json:"weather"`
+	Main struct {
+		Temp     int `json:"temp"`
+		Pressure int `json:"pressure"`
+		Humidity int `json:"humidity"`
+	} `json:"main"`
+	Sys struct {
+		Country string `json:"country"`
+	} `json:"sys"`
+}
+
 func main() {
 	res, err := http.Get("https://api.openweathermap.org/data/2.5/weather?q=Nairobi&appid=73cbc8ffd789d41ed5535c3fdb8ca562")
 	if err != nil {
